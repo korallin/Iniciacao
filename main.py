8c71a1b3fc2f74e3dcbae0b638f9c94c39dcb3d2
Yan Kelvin <yan.kelvin@hotmail.com>
2019-10-01 17:24:36 -0300

    Criação de classe para modularização do código
    Criação da classe Scrape para modularizar o código em funções distintas.

--- main.py
+++ main.py
@@ -3,50 +3,66 @@ from bs4 import BeautifulSoup as soup
 from pprint import pprint
 from re import findall, match
 
-url = 'https://www.tjse.jus.br/tjnet/consultas/internet/exibeIntegra.wsp?tmp.numProcesso=201740301376&tmp.dtMovimento=20170726&tmp.seqMovimento=1&tmp.codMovimento=371&tmp.tipoIntegra=2'
-url2 = 'https://www.tjse.jus.br/tjnet/consultas/internet/exibeIntegra.wsp?tmp.numProcesso=201740301380&tmp.dtMovimento=20170623&tmp.seqMovimento=2&tmp.codMovimento=371&tmp.tipoIntegra=2'
+urls = ['https://www.tjse.jus.br/tjnet/consultas/internet/exibeIntegra.wsp?tmp.numProcesso=201740301376&tmp.dtMovimento=20170726&tmp.seqMovimento=1&tmp.codMovimento=371&tmp.tipoIntegra=2',
+        'https://www.tjse.jus.br/tjnet/consultas/internet/exibeIntegra.wsp?tmp.numProcesso=201740301380&tmp.dtMovimento=20170623&tmp.seqMovimento=2&tmp.codMovimento=371&tmp.tipoIntegra=2']
 
-req = Request(url2, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-page_soup = soup(webpage, 'html.parser')
 
-dados_processo = page_soup.find_all('b')
-cabecalho = dict()
+class Scrape:
+    def __init__(self, url):
+        self.url = url
+        self.cabecalho = dict()
+        self.corpo = dict()
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        self.page_soup = soup(webpage, 'html.parser')
 
-for dado in dados_processo:
-    try:
-        key = dado.text.strip().replace(':', '')
-        info = dado.next_sibling.next_sibling.strip()
-        cabecalho[key] = info
-    except:
-        pass
+    def getCabecalho(self):
+        dados_processo = self.page_soup.find_all('b')
 
-cabecalho['Número'] = findall('sso=[0-9]*', url)[0].replace('sso=', '')
-
-corpo = dict()
-sentenca = page_soup.find_all(
-    'p', attrs={'class': 'western', 'align': 'center'})
-if len(sentenca) > 0 and sentenca[0].text == 'SENTENÇA':
-    corpo['SENTENÇA'] = page_soup.find_all(
-        'p', attrs={'style': 'margin-bottom: 0cm;'})
-else:
-    cabecalho['Juiz'] = page_soup.find_all(
-        'p', attrs={'style': 'font-style: normal; margin-bottom: 0.28cm;', 'align': 'justify'})[0].text
-
-    paragrafos = page_soup.find_all(
-        'p', attrs={'align': 'justify'})
-
-    for paragrafo in paragrafos:
-        if match('[IV]*\s.\s', paragrafo.text):
-            key = paragrafo.text.strip()
-            corpo[key] = ''
-        else:
+        for dado in dados_processo:
             try:
-                if paragrafo.text != '\xa0':
-                    corpo[key] += paragrafo.text
+                key = dado.text.strip().replace(':', '')
+                info = dado.next_sibling.next_sibling.strip()
+                self.cabecalho[key] = info
             except:
                 pass
 
+        self.cabecalho['Número'] = findall(
+            'sso=[0-9]*', self.url)[0].replace('sso=', '')
+        return self.cabecalho
 
-pprint(cabecalho)
-pprint(corpo)
+    def getCorpo(self):
+        sentenca = self.page_soup.find_all(
+            'p', attrs={'class': 'western', 'align': 'center'})
+        if len(sentenca) > 0 and sentenca[0].text == 'SENTENÇA':
+            self.getSetencaTipo1()
+        else:
+            self.getSetencaTipo2()
+        return self.corpo
+
+    def getSetencaTipo1(self):
+        print('Not implemented yet.')
+
+    def getSetencaTipo2(self):
+        self.cabecalho['Juiz'] = self.page_soup.find_all(
+            'p', attrs={'style': 'font-style: normal; margin-bottom: 0.28cm;', 'align': 'center'})[0].text
+
+        paragrafos = self.page_soup.find_all(
+            'p', attrs={'align': 'justify'})
+
+        for paragrafo in paragrafos:
+            if match('[IV]*\s.\s', paragrafo.text):
+                key = paragrafo.text.strip()
+                self.corpo[key] = ''
+            else:
+                try:
+                    if paragrafo.text != '\xa0':
+                        self.corpo[key] += paragrafo.text
+                except:
+                    pass
+        return self.corpo
+
+
+scrape = Scrape(urls[0])
+pprint(scrape.getCabecalho())
+pprint(scrape.getCorpo())
